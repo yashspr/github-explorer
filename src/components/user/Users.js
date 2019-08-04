5d0bac780c9bdbdcec44996779b0b296532a8d83
Yashas Reddy <yashassp@protonmail.com>
2019-08-04 21:51:23 +0530

    fixed error rergarding previous commit

--- src/components/user/Users.js
+++ src/components/user/Users.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserItem from '../user/UserItem';
+import Spinner from '../layout/Spinner';
 
 export function Users(props) {
 
@@ -14,8 +15,9 @@ export function Users(props) {
 				<input type="text" className="form-control mb-2 mr-sm-2" id="username_search" placeholder="Search for a user" />
 				<button type="submit" className="btn btn-primary mb-2" onClick={formSubmit}>Submit</button>
 			</form>
+			<Spinner />
 			<div className="row">
-				{this.props.users && this.props.users.map((user, index) => {
+				{props.users && props.users.map((user, index) => {
 					return (
 						<UserItem {...user} />
 					)
