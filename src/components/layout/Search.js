d584decdfaa85baf993a6f41bd54892d9440417e
Yashas Reddy <yashassp@protonmail.com>
2019-08-06 14:31:04 +0530

    added state to Search Component

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export function Search(props) {
+export class Search extends Component {
 
-	const formSubmit = (e) => {
-		e.preventDefault();
-		props.getUsers(document.querySelector('#username_search').value);
+	state = {
+		searchText: ""
 	}
 
-	return (
+	formSubmit = (e) => {
+		e.preventDefault();
+		if(this.state.searchText.trim() === "") {
+			this.props.clearUsers();
+		} else {
+			this.props.getUsers(document.querySelector('#username_search').value.trim());
+		}
+	}
+
+	onChange = (e) => {
+		this.setState({searchText: e.target.value.trim()})
+	}
+
+	render() {
+		return (
 			<>
-				<form className="form-inline row my-3">
-					<input type="text" className="col-md-9 col-sm-12 form-control mb-2 mr-sm-2" id="username_search" placeholder="Search for a user" />
-					<button type="submit" className="col-md-1 col-sm-12 btn btn-primary mb-2" onClick={formSubmit}>Submit</button>
+				<form className="form-inline row my-3" onSubmit={this.formSubmit}>
+					<input type="text" className="col-md-9 col-sm-12 form-control mb-2 mr-sm-2" id="username_search" placeholder="Search for a user" value={this.state.searchText} onChange={this.onChange} />
+					<button type="submit" className="col-md-1 col-sm-12 btn btn-primary mb-2" onClick={this.formSubmit}>Submit</button>
 				</form>
 			</>
 		)
+	}
 }
 
 export default Search
