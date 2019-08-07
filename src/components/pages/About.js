5a567f6f925e297967698c0d951a4b83e1256c3a
Yashas Reddy <yashassp@protonmail.com>
2019-08-08 00:28:59 +0530

    fixed type

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = (props) => {
 	return (
 		<div className="container mt-3">
 			<h1>About The Website</h1>
-			<p className="lead">This web app is used to search for github users using their name and see general iformation about them.</p>
+			<p className="lead">This web app is used to search for github users using their name and see general information about them.</p>
 		</div>
 	)
 }
